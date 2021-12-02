f965ee295ff4ea2fa641cf584f4341724737d9e8
Sherban-Kostya <sherbankostya86@gmail.com>
2021-12-02 16:20:55 +0200

    дз 15

--- 15/index.php
+++ 15/index.php
@@ -1,11 +1,18 @@
 <?php
 class User {
-    protected $name;
-    protected $age;
-    public $setName;
-    public $getName;
-    public $setAge;
-    public $getAge;
+
+  protected $name;
+  protected $age;
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
+
 }
 class Worker extends User {
     private $salary;
@@ -22,16 +29,28 @@ class Man extends User {
     public $age;
     public $salary;
 }
+class student extends User {
+    private $sholarship;
+    private $well;
+}
+class Driver extends Worker {
+    private $drivingexperience;
+    private $categories = ['A','B','C'];
+}
 $Ivan = new Man();
 $Ivan->name = 'Ivan';
 $Ivan->age = 25;
-$Ivan->Salary = 1000;
+$Ivan->salary = 1000;
 $Vasya = new Man();
 $Vasya->name = 'Vasya';
 $Vasya->age = 26;
 $Vasya->salary = 2000;
-echo 'name:' . $Ivan->name . ',age:' . $Ivan->age  . ',salary:' . $Ivan->salary . "\n";
-echo 'name:' . $Vasya->name . ',age:' . $Vasya->age  . ',salary:' . $Vasya->salary . "\n";
 
-
+$users = [$Ivan,$Vasya];
+$sum = 0;
+foreach ($users as $item) {
+    echo 'salary:' . $item->salary . "\n";
+    $sum += $item->salary;
+}
+echo 'Sum:' . $sum;
 
